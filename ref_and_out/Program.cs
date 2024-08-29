865f4a88498e31ffd9a13fc312b9ef83fe19f5fe
Mehranjamali123 <jamalikhan367@gmail.com>
2024-08-29 01:11:52 -0700

    implement

---
+++ ref_and_out/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_and_out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //use of ref 
+            int a =10;
+
+            implement(ref a);
+
+            Console.WriteLine( a);
+
+            //use of out
+
+            string name;
+            outmethod(out name);
+            Console.WriteLine(name);
+
+
+
+        }
+
+        static void implement(ref int a)
+        {
+            a = 30;
+        }
+
+        static void outmethod(out string name)
+        {
+            name = "mehran";
+            string res = name + "jamali";
+        }
+    }
+}
